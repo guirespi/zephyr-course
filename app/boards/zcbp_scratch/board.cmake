4c8726029e7b8f5fe70c1662273a2cf30fd83e1b
Guido Ramirez <guidoramirez1998@hotmail.es>
2026-09-24 22:47:55 -0500

    rename previous file

--- app/boards/zcbp_scratch/board.cmake
+++ app/boards/zcbp_scratch/board.cmake
@@ -1,4 +1,5 @@
-# SPDX-License-Identifier: Apache-2.0
+zephyr_library()
+zephyr_library_sources(board.c)
 
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
 board_runner_args(jlink "--device=STM32F401CE" "--speed=4000")
